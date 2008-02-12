f26ba576c55cdd35ea5191be9826e61c7e469d22
rjones@thinkpad.home.annexia.org <rjones@thinkpad.home.annexia.org>
2008-02-12 20:22:43 +0000

    Support for calendar >= 2.0

--- virt-top/virt_top_calendar1.ml
+++ virt-top/virt_top_calendar1.ml
@@ -16,7 +16,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-   This file contains all code which requires ocaml-calendar.
+   This file contains all code which requires ocaml-calendar < 2.0
 *)
 
 open Printf
