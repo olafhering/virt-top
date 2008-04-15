2feb3625d75f9f257fc04c7ef482bad1f9f6cb44
Richard W.M. Jones <rjones@redhat.com>
2008-04-15 15:33:30 +0100

    Empty *.mli files to stop those modules from exporting symbols.

---
+++ virt-df/virt_df_linux_swap.mli
@@ -0,0 +1,22 @@
+(* 'df' command for virtual domains.
+   (C) Copyright 2007-2008 Richard W.M. Jones, Red Hat Inc.
+   http://libvirt.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *)
+
+(* This file is empty to stop this plug-in from exporting any
+   symbols to other modules by accident.
+*)
